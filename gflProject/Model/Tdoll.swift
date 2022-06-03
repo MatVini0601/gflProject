e987d74669efbed9d9f6444c81cad135b26deb06
Matheus Vinicius <matheus1ro@hotmail.com>
2022-06-03 18:06:41 -0300

    Criação do metodo POST e teste

--- gflProject/Model/Tdoll.swift
+++ gflProject/Model/Tdoll.swift
@@ -6,20 +6,51 @@
 //
 
 import Foundation
+    
 
-struct Tdoll: Decodable{
+class TdollModel{
+    struct Tdoll: Decodable{
     let id: Int
     let image: String
     let name: String
     let manufacturer: String
     let type: TdollType
 
-    enum TdollType: Decodable{
-        case AR
-        case SMG
-        case ST
-        case MG
-        case HG
-        case RF
+        enum TdollType: String, Decodable{
+            case AR = "AR"
+            case SMG = "SMG"
+            case ST = "ST"
+            case MG = "MG"
+            case HG = "HG"
+            case RF = "RF"
+        }
+    }
+
+    func post(_ tdoll: Tdoll, completion: @escaping (_ isSuccess: Bool) -> Void){
+        guard let baseURL = URL(string: "http://localhost:3000/tdolls") else { return }
+        let tdoll: [String: Any] = [
+            "id" : tdoll.id,
+            "image": tdoll.image,
+            "name": tdoll.name,
+            "manufacturer": tdoll.manufacturer,
+            "type": tdoll.type.rawValue
+        ]
+        let body = try? JSONSerialization.data(withJSONObject: tdoll, options: [])
+        
+        var request = URLRequest(url: baseURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = body
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if error == nil {
+                completion(true)
+                return
+            }
+            completion(false)
+        }.resume()
     }
 }
+
+
+
--- gflProject/View/Home/Home.swift
+++ gflProject/View/Home/Home.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text("Hello, Tdolls")
+        Text("Post")
+            .onTapGesture {
+                let tdoll = TdollModel.Tdoll(id: 55, image: "Some Image", name: "M4A1", manufacturer: "16LAB", type: .AR)
+                TdollModel().post(tdoll) { isSuccess in
+                    if !isSuccess{
+                        print("deu errado")
+                    }
+                }
+            }
     }
 }
 
