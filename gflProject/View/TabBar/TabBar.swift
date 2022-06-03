bb141b2b7aab893d3031e00b48c2531831b94753
Matheus Vinicius <matheus1ro@hotmail.com>
2022-06-03 14:43:47 -0300

    Primeiro Commit

---
+++ gflProject/View/TabBar/TabBar.swift
@@ -0,0 +1,22 @@
+//
+//  TabBar.swift
+//  gflProject
+//
+//  Created by Matheus Vinicius Mota Rodrigues on 03/06/22.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView{
+            
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
---
+++ gflProject/View/Tdoll/List/TdollList.swift
@@ -0,0 +1,20 @@
+//
+//  TdollList.swift
+//  gflProject
+//
+//  Created by Matheus Vinicius Mota Rodrigues on 03/06/22.
+//
+
+import SwiftUI
+
+struct TdollList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TdollList_Previews: PreviewProvider {
+    static var previews: some View {
+        TdollList()
+    }
+}
