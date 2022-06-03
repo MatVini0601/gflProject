de89d69ac87eeb8cc9533ffe02a146c4d076e4ce
Matheus Vinicius <matheus1ro@hotmail.com>
2022-06-03 14:37:51 -0300

    Initial Commit

---
+++ gflProject/gflProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  gflProjectApp.swift
+//  gflProject
+//
+//  Created by Matheus Vinicius Mota Rodrigues on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct gflProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
