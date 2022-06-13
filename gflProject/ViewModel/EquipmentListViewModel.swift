869e0ce1f1d754a86793dd1e7045da9f212c38b9
Matheus Vinicius <matheus1ro@hotmail.com>
2022-06-13 18:59:55 -0300

    Criação da ViewModel de Equipamentos

---
+++ gflProject/ViewModel/EquipmentListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  EquipmentListViewModel.swift
+//  gflProject
+//
+//  Created by Matheus Vinicius Mota Rodrigues on 13/06/22.
+//
+
+import Foundation
+
+class EquipmentListViewModel: ObservableObject {
+    @Published var equipmentList = []
+}
