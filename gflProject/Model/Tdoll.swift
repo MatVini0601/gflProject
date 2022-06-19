152aa58e6a4ca3bd5b5dfbe07a5a27034e49f902
Matheus Vinicius <matheus1ro@hotmail.com>
2022-06-19 20:17:34 -0300

    Error types e validações

--- gflProject/Model/Tdoll.swift
+++ gflProject/Model/Tdoll.swift
@@ -19,37 +19,61 @@ class TdollModel{
         enum TdollType: String, Decodable{
             case AR = "AR"
             case SMG = "SMG"
-            case ST = "ST"
+            case SG = "SG"
             case MG = "MG"
             case HG = "HG"
             case RF = "RF"
         }
     }
-
-    func post(_ tdoll: Tdoll, completion: @escaping (_ isSuccess: Bool) -> Void){
-        guard let baseURL = URL(string: "http://localhost:3000/tdolls") else { return }
-        let tdoll: [String: Any] = [
-            "id" : tdoll.id,
-            "image": tdoll.image,
-            "name": tdoll.name,
-            "manufacturer": tdoll.manufacturer,
-            "type": tdoll.type.rawValue
-        ]
-        let body = try? JSONSerialization.data(withJSONObject: tdoll, options: [])
-        
-        var request = URLRequest(url: baseURL)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = body
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if error == nil {
-                completion(true)
-                return
-            }
-            completion(false)
-        }.resume()
+    
+    enum errorTypes: String{
+        case RequiredFieldEmpty = "Campos Obrigatorios não preenchidos"
+        case NegativeID = "Campo ID não pode ser menor que zero"
+        case ServerError = "Servidor: Erro ao tentar salvar nova Tdoll"
+        case NoError
     }
+
+    func post(_ tdoll: Tdoll, completion: @escaping (_ isSuccess: Bool,_ error: errorTypes) -> Void){
+    guard let baseURL = URL(string: "http://localhost:3000/tdolls") else { return }
+    let validation = validateTdoll(tdoll)
+    
+    switch(validation){
+        case .NoError:
+            completion(true, .NoError)
+            break
+        case .RequiredFieldEmpty:
+            completion(false, .RequiredFieldEmpty)
+            return
+        case .NegativeID:
+            completion(false, .NegativeID)
+            return
+        case .ServerError:
+            completion(false, .ServerError)
+            return
+    }
+    
+    let tdoll: [String: Any] = [
+        "id" : tdoll.id,
+        "image": tdoll.image,
+        "name": tdoll.name,
+        "manufacturer": tdoll.manufacturer,
+        "type": tdoll.type.rawValue
+    ]
+    let body = try? JSONSerialization.data(withJSONObject: tdoll, options: [])
+    
+    var request = URLRequest(url: baseURL)
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = body
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if error == nil {
+            completion(true, .NoError)
+            return
+        }
+        completion(false, .ServerError)
+    }.resume()
+}
     
     func getTdolls() async  throws -> [Tdoll]? {
         guard let request = setRequest(method: "GET", string: "http://localhost:3000/tdolls") else { return nil }
@@ -58,10 +82,10 @@ class TdollModel{
     }
     
     func search(_ search: String) async throws -> [Tdoll]? {
-        guard let request = setRequest(method: "GET", string: "http://localhost:3000/tdolls/search?name=\(search)") else { return nil }
-        guard let tdollList = try? await getData(with: request) else { return nil }
-        return tdollList
-    }
+    guard let request = setRequest(method: "GET", string: "http://localhost:3000/tdolls/search?name=\(search)") else { return nil }
+    guard let tdollList = try? await getData(with: request) else { return nil }
+    return tdollList
+}
     
     func getTdollByType(type: Tdoll.TdollType) async throws -> [Tdoll]? {
         guard let request = setRequest(method: "GET", string: "http://localhost:3000/tdolls/type/\(type.rawValue)") else { return nil }
@@ -77,7 +101,7 @@ class TdollModel{
     
     
     //Métodos auxiliares
-    func setRequest(method: String, string url: String) -> URLRequest?{
+    private func setRequest(method: String, string url: String) -> URLRequest?{
         guard let baseURL = URL(string: url) else { return nil}
         var request = URLRequest(url: baseURL)
         request.httpMethod = method
@@ -86,10 +110,16 @@ class TdollModel{
         return request
     }
 
-    func getData(with request: URLRequest) async throws -> [Tdoll]?{
+    private func getData(with request: URLRequest) async throws -> [Tdoll]?{
         let (data, _) = try await URLSession.shared.data(for: request)
         guard let tdollsList = try? JSONDecoder().decode([Tdoll].self, from: data) else { return [] }
         return tdollsList
+    }
+    
+    private func validateTdoll(_ tdoll: Tdoll) -> errorTypes {
+        if tdoll.id < 0 { return .NegativeID }
+        if tdoll.image.isEmpty || tdoll.name.isEmpty || tdoll.manufacturer.isEmpty { return .RequiredFieldEmpty }
+        return .NoError
     }
 }
 
