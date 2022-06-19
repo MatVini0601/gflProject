8b45e12521a398c36c5a9edf39f4e2b94970e86a
Matheus Vinicius <matheus1ro@hotmail.com>
2022-06-19 20:24:13 -0300

    Separação de arquivos

---
+++ gflProject/ViewModel/Tdoll/TdollPostViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  TdollPostViewModel.swift
+//  gflProject
+//
+//  Created by Matheus Vinicius Mota Rodrigues on 19/06/22.
+//
+
+import Foundation
+
+class TdollPostViewModel: ObservableObject {
+    var alertMessage = ""
+    
+    func postTdoll(_ tdoll: TdollModel.Tdoll){
+        TdollModel().post(tdoll) { isSuccess, error  in
+            if isSuccess{ self.alertMessage = "Tdoll salva com sucesso" }
+            else { self.alertMessage = error.rawValue }
+        }
+    }
+}
