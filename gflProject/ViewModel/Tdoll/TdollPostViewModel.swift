148b87ccaa9d87fde341694a3f4e3a14243fa471
Matheus Vinicius <matheus1ro@hotmail.com>
2022-07-09 20:05:03 -0300

    Criação de método assíncrono

--- gflProject/ViewModel/Tdoll/TdollPostViewModel.swift
+++ gflProject/ViewModel/Tdoll/TdollPostViewModel.swift
@@ -11,12 +11,13 @@ class TdollPostViewModel: ObservableObject {
     var alertMessage = ""
     var ErrorType: TdollModel.errorTypes = .NoError
     
-    func postTdoll(_ tdoll: TdollModel.Tdoll){
-        TdollModel().post(tdoll) { isSuccess, error  in
-            if isSuccess{ self.alertMessage = "Tdoll salva com sucesso" }
+    func postTdoll(_ tdoll: TdollModel.Tdoll) async throws -> Void{
+        try await TdollModel().post(tdoll) { isSuccess, error  in
+            if isSuccess{ self.alertMessage = "Tdoll salva com sucesso"; return }
             else {
                 self.alertMessage = error.rawValue
                 self.ErrorType = error
+                return
             }
         }
     }
