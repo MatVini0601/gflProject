f59bf3114c9500f62d4c8f7fa87019665db7b29e
Matheus Vinicius Mota <matheus000020@gmail.com>
2022-12-28 21:38:52 -0300

    Separação de responsabilidade

---
+++ gflProject/View/GalleryView.swift
@@ -0,0 +1,74 @@
+//
+//  GalleryView.swift
+//  gflProject
+//
+//  Created by Matheus Vinicius Mota Rodrigues on 14/12/22.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    @Namespace var TdollDetails
+    @EnvironmentObject var ViewModel: GalleryViewModel
+    @State var ID: Int
+    @State var refresh = false
+    
+    init(ID: Int, ViewModel: GalleryViewModel = GalleryViewModel()) {
+        self.ID = ID
+    }
+    
+    var body: some View {
+        if ViewModel.tdollHasGallery(){
+            VStack{
+                Text("Gallery")
+                    .font(.custom("Montserrat", size: 24))
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack{
+                        ForEach(ViewModel.gallery, id: \.self){ item in
+                            Text(item.model_name)
+                                .padding(.vertical, 4)
+                                .padding(.horizontal, 8)
+                                .background{
+                                    Capsule().stroke(lineWidth: 1).fill(Color.LightGray)
+                                    if ViewModel.skinNameSelection == item.model_name{
+                                        Capsule()
+                                            .fill(Color.lightYellow)
+                                            .matchedGeometryEffect(id: "SKIN", in: TdollDetails)
+                                    }
+                                }
+                                .lineLimit(1)
+                                .onTapGesture {
+                                    withAnimation{ViewModel.skinNameSelection = item.model_name}
+                                    ViewModel.skinImageLink = item.image_link
+                                }
+                        }
+                    }
+                    .padding(1)
+                }
+                
+                AsyncImage(url: URL(string: ViewModel.skinImageLink)){image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                    }placeholder: {
+                        ProgressView()
+                            .frame(width: 100, height: 200, alignment: .center)
+                            .scaledToFill()
+                    }
+            }
+            .padding(.horizontal)
+            .task {
+                await ViewModel.getTdollGallery(id: self.ID)
+                refresh = true
+            }
+        }
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider  {
+    static var previews: some View {
+        GalleryView(ID: 57)
+            .environmentObject(GalleryViewModel())
+    }
+}
---
+++ gflProject/ViewModel/Tdoll/TdollDetailsViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  TdollDetailsViewModel.swift
+//  gflProject
+//
+//  Created by Matheus Vinicius Mota Rodrigues on 06/12/22.
+//
+
+import Foundation
+
+class TdollDetailsViewModel: ObservableObject {
+    private let Model = TdollModel()
+    private let TagModel = Tags()
+    
+    private let LAB = "16LAB Research Institute (also rendered “16Lab”) is the company created by Persicaria in 2057, and one of IOP's first-party manufacturer and R&D partners in the Tactical Doll market."
+    private let IOP = "Important Operations Prototype Manufacturing Company, or IOP, is a R&D and manufacturing company specializing in Tactical Dolls, directed by Havier Witkin. IOP is the world leader in Tactical Doll production since World War Three thanks to 16LAB's groundbreaking technological research, and the main supplier of workforce for Griffin & Kryuger PMC."
+    private let Unknown = "This doll is not originally from the game. it`s a character from a collab so it`s either don`t have a manufacturer or we don`t know who they are."
+    var alertMessage = ""
+    var ErrorType: NetworkManager.errorTypes = .NoError
+
+    //MARK: Tdoll
+    func deleteTdoll(id: Int) async {
+        NetworkManager.shared.deleteTdoll(id) { deleted, error in
+            if deleted { self.alertMessage = "Tdoll excluída com sucesso"; return}
+            else {
+                self.alertMessage = error.rawValue
+                self.ErrorType = .DeleteError
+                return
+            }
+        }
+    }
+    
+    //MARK: Tags
+    func getTdollTags(id: Int)async -> [Tags.tagData]{
+        guard let tags = try? await TagModel.getTags(id) else { return [] }
+        return tags
+    }
+    func tdollHasTags() -> Bool{
+        return TagModel.hasTags()
+    }
+    
+    //MARK: Aux
+    func getLAB() -> String{ return self.LAB }
+    func getIOP() -> String{ return self.IOP }
+    func getUnknow() -> String { return self.Unknown }
+}
+
